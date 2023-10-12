28515232e5889f2ae3b4a5fcb856ba074aa71af1
nickkatsy <141574717+nickkatsy@users.noreply.github.com>
2023-10-11 22:37:56 -0400

    Update New_Alexandria_10000_times.py

--- New_Alexandria_10000_times.py
+++ New_Alexandria_10000_times.py
@@ -1,14 +1,14 @@
 import random
 from collections import Counter
 
-# Number of times you played this stupid level withot reverting
-num_trials = 1000
+# Number of times you played this stupid level without reverting
+num_trials = 10000
 
 # Lists to store results for each trial
 results = []
 
 # Sequence to track
-sequence_to_track = ["Hospital", "Whiskey 39er", "Vyrant", "Foxtrot", "Sinoviet"]
+the_best_rng = ['Hospital', 'Whiskey 39er', 'Vyrant', 'Foxtrot', 'Sinoviet']
 
 # Initialize counter for the sequence
 sequence_count = 0
@@ -21,10 +21,10 @@ for _ in range(num_trials):
 
     # Side objectives grouped by categories
     side_objectives = {
-        "Helping Marines": ['Vs. Brutes', 'Foxtrot', 'Vs. Jackals'],
-        "3 Enemies": ['Whiskey 39er', 'Banshees', 'Snipers'],
-        "Covenant Causing Trouble": ['Elites and Engineers', 'Shades', 'Jammer'],
-        "Buck": ['Buck', 'Buck2', 'Buck3']
+        'Helping Marines': ['Vs. Brutes', 'Foxtrot', 'Vs. Jackals'],
+        '3 Enemies': ['Whiskey 39er', 'Banshees', 'Snipers'],
+        'Covenant Causing Trouble': ['Elites and Engineers', 'Shades', 'Jammer'],
+        'Buck': ['Buck', 'Buck2', 'Buck3']
     }
 
     # Select one side objective from the 12 available
@@ -34,7 +34,7 @@ for _ in range(num_trials):
     objectives.append(selected_side_objective)
 
     # main objective based on a coin flip(50/50 dudde)
-    main_objective = random.choice(["Sinoviet", "Vyrant"])
+    main_objective = random.choice(['Sinoviet', 'Vyrant'])
     objectives.append(main_objective)
 
     # Select one more side objective from the remaining 9
@@ -45,14 +45,14 @@ for _ in range(num_trials):
     objectives.append(random.choice(remaining_side_objectives))
 
     # Determine the last main objective based on the previous main objective
-    last_main_objective = "Sinoviet" if main_objective == "Vyrant" else "Vyrant"
+    last_main_objective = 'Sinoviet' if main_objective == 'Vyrant' else 'Vyrant'
     objectives.append(last_main_objective)
 
     # Append the generated objectives to the results
     results.append(objectives)
 
     # Check for the sequence in the objectives
-    if objectives == sequence_to_track:
+    if objectives == the_best_rng:
         sequence_count += 1
 
 # Count the frequency of objectives for all trials
@@ -60,9 +60,9 @@ all_objectives = [objective for objectives in results for objective in objective
 most_common_objectives = Counter(all_objectives).most_common()
 
 # Display the most frequent objectives
-print("Most Frequent Objectives:")
+print('Most Frequent Objectives:')
 for objective, frequency in most_common_objectives:
-    print(f"{objective}: {frequency} times")
+    print(f'{objective}: {frequency} times')
 
 # Display the count of the sequence
-print(f"\nCount of Sequence '{sequence_to_track}': {sequence_count} times")
+print(f'\nCount of Sequence {the_best_rng}: {sequence_count} times')
