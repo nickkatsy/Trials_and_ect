9e0eea6fd134cffccdf8910a71ddfe99e1cca93a
nickkatsy <141574717+nickkatsy@users.noreply.github.com>
2023-10-15 10:23:40 -0400

    Update poly_iter_returns.py

--- poly_iter_returns.py
+++ poly_iter_returns.py
@@ -3,8 +3,10 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import PolynomialFeatures
+import warnings
+warnings.filterwarnings('ignore')
 
-sp500 = "^GSPC"
+sp500 = '^GSPC'
 
 tickers = ['AAPL', 'GOOG', 'T', 'F', 'AMZN']
 
